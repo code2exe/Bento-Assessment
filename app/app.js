d6eccb6d11d342e593fe359ad91dbc0371bf12d4
Henry <16284830+code2exe@users.noreply.github.com>
2020-09-16 01:04:40 +0000

    App Setup

--- app/app.js
+++ app/app.js
@@ -1,14 +1,20 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000;
+const timestamp = Date.now();
+let date_ob = new Date(timestamp);
+let date = ("0" + date_ob.getDate()).slice(-2);
+let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+let year = date_ob.getFullYear();
+let hours = date_ob.getHours();
+let minutes = date_ob.getMinutes();
+let seconds = date_ob.getSeconds();
+console.log(year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds);
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello There');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+//Define request response in root URL (/)
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+//Launch listening server on port 3000
+// const port = PORT || 3000;
+app.listen(port, () => console.log(`Server listening on port ${port}`));
