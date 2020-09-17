96d85c187344a3e704613fbdfc535cc0a7017030
Henry <16284830+code2exe@users.noreply.github.com>
2020-09-17 16:49:29 +0000

    Fixes

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-terraform = {
-    backend "s3" {
-        bucket = "bytelabs"
-        key = "terraform.tfstate" 
-        region = "eu-west-2"
-        encrypt = true
-    }
+terraform {
+  backend "s3" {
+    bucket = "bytelabs"
+    key    = "terraform.tfstate"
+    region = "eu-west-2"
+    encrypt = true
+  }
 }
