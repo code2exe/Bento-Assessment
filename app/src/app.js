70bfe3b5214a4cef3b8b5e251c1a022da23c1f78
Henry <16284830+code2exe@users.noreply.github.com>
2020-09-18 17:48:25 +0000

    Refactor: Made timestamp dynamic

--- app/src/app.js
+++ app/src/app.js
@@ -2,18 +2,9 @@ const express = require('express')
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 3000;
-let ts = Date.now();
-let time = new Date(ts)
-let year = time.getFullYear()
-let month = time.getMonth()
-let date = time.getDate()
-let hours = time.getHours()
-let minutes = time.getMinutes()
-let seconds = time.getSeconds()
-let timestamp = `${year}-${month}-${date} ${hours}:${minutes}:${seconds}`;
 
 //Define request response in root URL (/)
 app.get('/', function (req, res) {
-  res.send(timestamp)
+  res.send(new Date(Date.now()).toLocaleString())
 })
 app.listen(port, () => console.log(`Server listening on port ${port}`));
